e677a3c52774fb97b033e478ae71bfb3b7797223
Tyler <tydcarl@gmail.com>
2025-05-06 11:51:03 -0700

    what lies ahead

--- user.js
+++ user.js
@@ -1,21 +1,31 @@
 const postListEl = document.querySelector(".post-list");
+  const id = localStorage.getItem("id");
 
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-    console.log(postsData)
+async function onSearchChange(event) {
+    const  id = event.target.value; 
+    renderPosts(id)
 
-    postListEl.innerHTML = postsData.map(
-    post => `<div class="post">
-      <div class="post__title">
-        ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body} 
-      </p>
-    </div>  `
-    ).join('')
+} 
+
+async function renderPosts(id) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  const postsData = await posts.json();
+  postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
 }
 
-main();
+function postHTML(post) {
+     return `
+      <div class="post">
+      <div class="post__title">
+      ${post.title}
+      </div>
+      <p class="post__body">
+      ${post.body}
+      </p>
+      </div>
+      `
+
+}
+
+renderPosts(id)
+ 
